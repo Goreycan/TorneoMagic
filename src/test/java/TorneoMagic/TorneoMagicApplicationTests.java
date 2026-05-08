8a9c0243444d6c1c4dddb02e200a83ae3a2ef4ac
Goreycan <goreyc90@gmail.com>
2026-05-08 19:15:14 -0400

    crear proyecto spring boot

---
+++ src/test/java/TorneoMagic/TorneoMagicApplicationTests.java
@@ -0,0 +1,13 @@
+package TorneoMagic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorneoMagicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
