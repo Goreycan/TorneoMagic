1abb363208d5c1235a81b446a0f0e1a527946212
kennortega-debug <kenn.ortega@duocuc.cl>
2026-05-11 10:11:20 -0400

    se crea comuna, local, organizador, local_organizador

---
+++ src/main/java/TorneoMagic/repository/ComunaRepository.java
@@ -0,0 +1,9 @@
+package TorneoMagic.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import TorneoMagic.model.Comuna;
+
+public interface  ComunaRepository extends JpaRepository<Comuna, Long> {
+
+}
---
+++ src/main/java/TorneoMagic/repository/LocalRepository.java
@@ -0,0 +1,9 @@
+package TorneoMagic.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import TorneoMagic.model.Local;
+
+public interface LocalRepository extends JpaRepository<Local, Long> {
+
+}
