53bc9ab9edcd466d31c7f7989efecd6f6d434166
Goreycan <goreyc90@gmail.com>
2026-05-11 16:46:10 -0400

    correccion mvn clean compile

---
+++ src/main/java/TorneoMagic/TorneoMagicApplication.java
@@ -0,0 +1,12 @@
+package TorneoMagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorneoMagicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TorneoMagicApplication.class, args);
+    }
+}
